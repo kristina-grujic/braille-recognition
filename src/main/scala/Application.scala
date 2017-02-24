e40d106dec71b3a5f2383f8dd2b1cf690c4ba9bc
Kristina G <pinksugarpill@gmail.com>
2017-02-24 08:35:42 +0100

    Image to binary matrix

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -7,5 +7,4 @@ object Application extends App {
 	val image = imagePreprocessor.preprocessor()
 	image.output(new File("C://Users/Windows/Downloads/result.png"))
 	val binary = new BinaryToLetter()
-	println(binary.getLetter(Array(0,1,1,1,1,0)))
 }
--- src/main/scala/preprocessing/ImagePreprocessor.scala
+++ src/main/scala/preprocessing/ImagePreprocessor.scala
@@ -7,23 +7,37 @@ import com.sksamuel.scrimage.filter.ThresholdFilter;
 
 class ImagePreprocessor(val inFile:String) {
   var image = Image.fromFile(new File(inFile));
-  var array = new ArrayBuffer[Int]();
+  var array = Array.ofDim[Int](image.width, image.height);
+  var counterh = 0;
+  var counterw = 0;
   
   def preprocessor() : Image = {
+    image = image.resizeTo(image.width, image.height);
     image = image.map(grayscaleThreshold);
+    
+    array.foreach( x => println(x.mkString) )
+    
     return image
   }
   
   private def grayscaleThreshold(x: Int, y: Int, pixel: Pixel) : Pixel = {
+    var result = pixel;
     var value = (pixel.red * 0.21 + pixel.green * 0.72 + pixel.blue * 0.07)/255
     if (value > 0.5) {
-      array += 0;
-      return Pixel(0, 0, 0, 255)
+      array(counterh)(counterw) = 0;
+      result = Pixel(0, 0, 0, 255)
     } else {
-      array += 1;
-      return Pixel(255, 255, 255, 255)
+      array(counterh)(counterw) = 1;
+      result = Pixel(255, 255, 255, 255)
     }
-    return pixel;
+     counterw += 1;
+      if (counterw == image.height) {
+        counterh += 1;
+        counterw = 0;
+      }
+      
+     
+    return result;
   }
  
   
